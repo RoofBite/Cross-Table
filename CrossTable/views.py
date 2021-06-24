70ee75241baaa6fa69ca241cff80b59d4be5c262
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-06-24 11:37:31 +0200

    Implementing table output

--- CrossTable/views.py
+++ CrossTable/views.py
@@ -28,8 +28,17 @@ def setup0(request):
         kwargs = {'quesion_0_possible_answers_number': quesion_0_possible_answers_number,'questions_number':questions_number}
         return redirect( 'CrossTable:setup1',**kwargs)
     else:
-        print("NIE działa")
-        return render(request,'CrossTable/setup0.html')
+        
+        temp_column="\tALL ANSWERS FOR MAIN QUESTION\tPERSON DOESN'T HAVE CAT\tPERSON LIVES IN NEW YORK\tQuestion 3\r\nYES\t20,0%\t22,2%\t25,0%\t25,0%\r\nNO\t25,0%\t33,3%\t12,5%\t25,0%\r\nI DON'T KNOW\t15,0%\t11,1%\t25,0%\t12,5%\r\nMAYBE\t15,0%\t11,1%\t12,5%\t12,5%\r\nANSWER 5\t10,0%\t11,1%\t12,5%\t12,5%\r\nANSWER 6\t15,0%\t11,1%\t12,5%\t12,5%\r\n"
+        temp_column = temp_column.split("\r\n")
+        temp_column0=[]
+        for item in temp_column:
+            x = item.split("\t")
+            temp_column0.append(x)
+        temp_column=temp_column0
+        print(temp_column)
+        context={'temp_column':temp_column}
+        return render(request,'CrossTable/setup0.html',context)
 
 def result(request,quesion_0_possible_answers_number_int,questions_number_int):
     #Root question + other questions
@@ -53,8 +62,10 @@ def result(request,quesion_0_possible_answers_number_int,questions_number_int):
     
    
     columns.append((request.POST.get('quesion_0_column')).split("\r\n"))
+    print(repr(request.POST.get('quesion_0_column')))
     
-    
+
+
     for i in range(1,questions_number_int+1):
         column=request.POST.get(f'quesion_{i}_column')
         columns.append(column.split("\r\n"))
